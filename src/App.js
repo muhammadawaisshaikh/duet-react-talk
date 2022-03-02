57ba4401b09b8b034cdf1264a2bf2e1d78fdff9f
Muhammad Awais <awaisshaikh94@gmail.com>
2022-03-02 10:12:45 +0500

    few changes for router on root

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import Header from './layout/Header';
 import About from './components/about/About';
 import Homepage from './components/homepage/Homepage';
 import Login from './components/auth/Login';
-import Signup from './components/auth/Login';
+import Signup from './components/auth/Signup';
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Header/ >
+      <Header/ >
 
-        <Routes>
-          <Route path="" element={<Homepage />} />
-          <Route path="homepage" element={<Homepage />} />
-          <Route path="about" element={<About />} />
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="" element={<Homepage />} />
+        <Route path="homepage" element={<Homepage />} />
+        <Route path="about" element={<About />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+      </Routes>
     </div>
   );
 }
